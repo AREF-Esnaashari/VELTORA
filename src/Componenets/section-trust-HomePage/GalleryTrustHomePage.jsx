9e1a7bd1e3ad189409acd120a96c9c8af6e5d6ec
AREF ESNAASHARI <its.esi2009@gmail.com>
2026-08-17 19:18:31 +0330

    description & Gallery trust complite

---
+++ src/Componenets/section-trust-HomePage/GalleryTrustHomePage.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+export default function GalleryTrustHomePage({ DataTrust, indexImg }) {
+  let [currentImg, setCurrentImg] = useState('');
+  let [isAvalable, setIsAvable] = useState(true);
+  useEffect(() => {
+    let isCurentImg = DataTrust.find((img) => {
+      return img.index === indexImg;
+    });
+
+    setIsAvable(false);
+    let timeout = setTimeout(() => {
+      setCurrentImg(isCurentImg.img);
+      setIsAvable(true);
+    }, 500);
+    () => {
+      clearTimeout(timeout);
+    };
+  }, [indexImg, DataTrust, setIsAvable]);
+
+  return (
+    <div className="p-10 sm:p-0 md:w-full lg:w-1/2  flex justify-center items-center">
+      <img
+        src={currentImg}
+        alt=""
+        className={`w-full max-w-lg h-[420px] object-cover rounded-2xl shadow-2xl shadow-black/40 transition-opacity duration-200 ease-in-out
+       ${isAvalable ? 'opacity-100' : 'opacity-25'}`}
+      />
+    </div>
+  );
+}
