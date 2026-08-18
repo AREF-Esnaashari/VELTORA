28bdff686fe0ea1bf859c14fb4dcbd66353c6b50
AREF ESNAASHARI <its.esi2009@gmail.com>
2026-08-18 14:25:43 +0330

    navigation app

--- src/Componenets/headers/Navbar.jsx
+++ src/Componenets/headers/Navbar.jsx
@@ -3,7 +3,12 @@ import Logo from './Logo';
 export default function Navbar() {
   return (
     <nav className="container h-20  mt-0 mx-auto">
-      <ul className=" flex gap-5 justify-evenly  items-center text-white mt-2 ">
+      <ul
+        className=" flex gap-5 justify-evenly  items-center text-white mt-2 "
+        data-aos="fade"
+        data-aos-duration="1500"
+        data-aos-easing="ease-out-cubic"
+      >
         <li>
           <NavLink>خانه</NavLink>
         </li>
