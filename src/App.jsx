04a90be2dd2b4f508ae27bd0318e87d193aedb70
AREF ESNAASHARI <its.esi2009@gmail.com>
2026-08-18 14:25:20 +0330

    Base Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
-
 import Home from './pages/Home';
-
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // استایل‌های ضروری AOS
 export default function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // مدت زمان انیمیشن (میلی‌ثانیه)
+      once: true, // انیمیشن فقط یک‌بار هنگام اسکرول اجرا شود
+    });
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
