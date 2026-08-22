726f14166fc9259e65be79bd9e8d9a7100f2cea0
AREF ESNAASHARI <its.esi2009@gmail.com>
2026-08-22 18:45:09 +0330

    Logo

--- src/Componenets/headers/Logo.jsx
+++ src/Componenets/headers/Logo.jsx
@@ -1,8 +1,8 @@
 export default function Logo() {
   return (
-    <h1 className="text-2xl text-center font-syne font-extrabold uppercase ">
+    <h1 className="text-2xl text-center font-syne font-bold uppercase ">
       Veltora
-      <p>ولتورا</p>
+      <p className="font-mono">ولتورا</p>
     </h1>
   );
 }
