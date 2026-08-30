85e806e6c706bf5401017f7caabd614e8f8e6949
AREF ESNAASHARI <its.esi2009@gmail.com>
2026-08-30 23:06:39 +0330

    Complite Products CP(dont have API)

--- src/Componenets/PRODUCTS PAGE/ProductsHeader.jsx
+++ src/Componenets/PRODUCTS PAGE/ProductsHeader.jsx
@@ -1,15 +1,15 @@
 export default function ProductsHeader() {
   return (
     <header className="flex flex-col items-center gap-5 pt-12 text-center">
-      {/* برچسب کوچک بالای عنوان */}
-      <span className="inline-flex items-center gap-2 rounded-full border border-[#EF9F27]/30 bg-[#EF9F27]/10 px-4 py-1.5 text-xs font-medium tracking-wide text-[#EF9F27] shadow-[0_0_20px_-5px_rgba(239,159,39,0.4)] backdrop-blur-md">
-        <span className="h-1.5 w-1.5 animate-pulse rounded-full bg-[#EF9F27] shadow-[0_0_8px_#EF9F27]" />
-        کالکشن اختصاصی Veltora
+
+      <span className="inline-flex items-center gap-2 rounded-full border border-[#EF9F27]/30 bg-[#EF9F27]/10 px-4 py-1.5 text-xs font-medium tracking-tighter text-[#EF9F27] shadow-[0_0_20px_-5px_rgba(239,159,39,0.4)] backdrop-blur-md">
+        <span className="h-1.5 w-1.5 animate-pulse rounded-full bg-[#EF9F27]" />
+        کالکشن اختصاصی VELTORA
       </span>
 
       <div className="flex items-center justify-center gap-4 sm:gap-6">
         <span className="h-px w-16 bg-gradient-to-l from-transparent to-[#EF9F27] sm:w-32" />
-        <h1 className="bg-gradient-to-l from-white via-white to-[#EF9F27] bg-clip-text text-4xl  whitespace-nowrap text-transparent p-3 drop-shadow-lg sm:text-6xl text-2xl text-center font-vazir font-bold uppercase">
+        <h1 className="bg-gradient-to-l from-white  to-[#EF9F27] bg-clip-text text-4xl  whitespace- text-transparent p-3 drop-shadow-lg sm:text-6xl text-2xl text-center font-vazir font-bold uppercase">
           محصولات ما
         </h1>
         <span className="h-px w-16 bg-gradient-to-r from-transparent to-[#EF9F27] sm:w-32" />
