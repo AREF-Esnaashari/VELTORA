df2a2fe42b860ba01aa50a3f1ff47f9ff3d42d68
AREF ESNAASHARI <its.esi2009@gmail.com>
2026-08-30 23:04:59 +0330

    Complite Footer CP

--- src/Componenets/HOME PAGE/section-footer-HomePage/Footer.jsx
+++ src/Componenets/HOME PAGE/section-footer-HomePage/Footer.jsx
@@ -4,7 +4,7 @@ import { FooterLinks } from './FooterLinks';
 
 export const Footer = () => {
   return (
-    <footer className="w-full bg-[#1E1E1E] pt-12 pb-8 dir-rtl" dir="rtl">
+    <footer className="w-full bg-[#1E1E1E] rounded-2xl pt-12 pb-8 dir-rtl" dir="rtl">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <CtaSection />
         <FooterLinks />
